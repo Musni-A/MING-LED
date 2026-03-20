6e1fa8c6bceeabbc96adeb59ca6257ed6ba12d0c
Musni Ahamed <ahmusni0@gmail.com>
2026-03-20 11:53:42 +0530

    Rewrite all

---
+++ client/src/pages/settings.jsx
@@ -0,0 +1,18 @@
+import NavBar from "./navBar";
+import TopBar from "./topBar";
+import { useState } from "react";
+
+export default function Settings(){
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+        return<>
+        <div  className=" flex h-screen bg-[#e0edfa] font-sans overflow-hidden relative">
+            <NavBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            <div className="flex-1 flex flex-col overflow-hidden min-w-0">
+                <TopBar setSidebarOpen={setSidebarOpen}/>
+                <div className="h-full" onClick={() =>setSidebarOpen(false)}>
+                    <h1>Settings</h1>
+                </div>
+            </div>
+        </div>
+        </>
+    }
